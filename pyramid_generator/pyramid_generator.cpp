d2a71f21cfe31daa72259392ea9cae21a2c98b21
MD. Sajid Hossain <69637820+sajidrec@users.noreply.github.com>
2021-04-20 16:36:16 +0600

    no change just format the code

--- pyramid_generator/pyramid_generator.cpp
+++ pyramid_generator/pyramid_generator.cpp
@@ -2,39 +2,38 @@
 
 using namespace std;
 
-int main(void){
+int main(void) {
 
+  long long int n, i, j, k;
 
-    long long int n, i, j, k;
+  char sChar;
 
-    char sChar;
+  while (true) {
 
-    while(true){
+    cout << "Enter row number or enter zero or any negative number to exit : ";
+    cin >> n;
 
-		cout << "Enter row number or enter zero or any negative number to exit : ";
-		cin >> n;
-
-		if(n <= 0){
-			cout << endl << endl << "Thx for using this program." << endl <<"This program was fully written by MD. Sajid Hossain but some concept has been copied from other resources." << endl << endl;
-			break;
-		}
-
-		cout << "Enter a character to design pyramid : ";
-		cin >> sChar;
-
-		for(i=1; i<n; i++){
-
-			for (k = 1; k <= n - i; k++) {
-				cout << " ";
-			}
-
-            for(j=1; j<=2*i-1; j++){
-				cout << sChar;
-            }
-            cout << endl;
-		}
-
+    if (n <= 0) {
+      cout << endl << endl << "Thx for using this program." << endl << "This program was fully written by MD. Sajid Hossain but some concept has been copied from other resources." << endl << endl;
+      break;
     }
 
-	return 0;
+    cout << "Enter a character to design pyramid : ";
+    cin >> sChar;
+
+    for (i = 1; i < n; i++) {
+
+      for (k = 1; k <= n - i; k++) {
+        cout << " ";
+      }
+
+      for (j = 1; j <= 2 * i - 1; j++) {
+        cout << sChar;
+      }
+      cout << endl;
+    }
+
+  }
+
+  return 0;
 }
